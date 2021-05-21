28aaea54eb1b52ac9bd6e8faad7df27765cd4d49
Xhoni <Xhonikomini96@gmail.com>
2021-05-21 11:40:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ module.exports = {
 			"links.papareact.com",
 			"platform-lookaside.fbsbx.com",
 			"firebasestorage.googleapis.com",
-			"facebook.com"
+			"facebook.com",
+			"https://www.facebook.com/v7.0/dialog/oauth?response_type=code&state=4a08ab98413cee8e11623346f3ac7e5e0cdf433b0a510314283475255a492d47&redirect_uri=https%3A%2F%2Ffacebook-clone-mn50canzw-xhoni96.vercel.app%2Fapi%2Fauth%2Fcallback%2Ffacebook&scope=email&client_id=467113151041428"
 		],
 	},
 };
